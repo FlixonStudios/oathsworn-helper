99ee1caebf3b1714ace402a763e3c3b3f5691386
FlixonStudios <chiakaiming@gmail.com>
2024-08-29 21:15:55 +0800

    add some babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      "babel-preset-expo",
+      "@babel/preset-typescript", // Add TypeScript support
+    ],
+    env: {
+      test: {
+        presets: [
+          ["@babel/preset-env", { targets: { node: "current" } }],
+          "@babel/preset-typescript", // Add TypeScript support in test environment
+        ],
+      },
+    },
   };
 };
