c178a100daccf816656694b7d3f3a25c3cc64afa
FlixonStudios <chiakaiming@gmail.com>
2024-10-04 14:01:13 +0800

    encode output to single line

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -2,7 +2,7 @@
 # run from project root
 
 # For a .json file (replace <file> with the path to your file):
-base64 google-services.json > google-services.json.base64
+base64 -w 0 google-services.json > google-services.json.base64
 
 # For a .keystore file:
-base64 release.keystore > release.keystore.base64
+base64 -w 0 release.keystore > release.keystore.base64
