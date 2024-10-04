c6b973c8cefdebd16aa4092a78be6fd201859ba5
FlixonStudios <chiakaiming@gmail.com>
2024-10-04 16:02:11 +0800

    inject secrets and fix remote signing script

--- scripts/set-remote-signing.sh
+++ scripts/set-remote-signing.sh
@@ -1,10 +1,11 @@
 # run from project root
 BUILD_GRADLE_PATH="android/app/build.gradle"
 
-# Read the store and key passwords from GitHub Secrets
-STORE_PASSWORD=${{ secrets.STORE_PASSWORD }}
-KEY_PASSWORD=${{ secrets.KEY_PASSWORD }}
+# base64 values (GitHub Secrets) should be injected to env via pipeline
+echo $GOOGLE_SERVICES_JSON_BASE64 | base64 --decode > ./google-services.json
+echo $RELEASE_KEYSTORE_BASE64 | base64 --decode > ./release.keystore
 
+# store and key passwords (GitHub Secrets) should be injected to env via pipeline
 sed -i "/signingConfigs.*{/a \\
         release {\\
             storeFile file('../../release.keystore')\\
