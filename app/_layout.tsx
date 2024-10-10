d429e06b0423837a7b75c112db31a1fbaf40e332
FlixonStudios <chiakaiming@gmail.com>
2024-10-10 15:28:43 +0800

    wrap root layout with settingsProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
+import { SettingsProvider } from "@/context-providers/settings/settings-provider";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: true, title: "Game Probability Advisor (Alpha)" }}
-      />
-    </Stack>
+    <SettingsProvider>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: true,
+            title: "Game Probability Advisor (Alpha)",
+          }}
+        />
+      </Stack>
+    </SettingsProvider>
   );
 }
