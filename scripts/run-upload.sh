f49fe8c7cd1944cce1937694f2eeae25978c8941
FlixonStudios <chiakaiming@gmail.com>
2024-10-12 21:07:36 +0800

    fix fastfile

--- scripts/run-upload.sh
+++ scripts/run-upload.sh
@@ -3,6 +3,8 @@ cd android
 
 # run init to get metadata
 bundle exec fastlane supply init
+
+echo "Starting upload to playstore"
 # execute upload to internal testing track
 bundle exec fastlane android upload
 
