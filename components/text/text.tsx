8a652beed87661da46e81a2a7f44db5780bb3238
FlixonStudios <chiakaiming@gmail.com>
2024-10-14 09:03:56 +0800

    rename Text in shared text to prepare for css standardisation

--- components/text/text.tsx
+++ components/text/text.tsx
@@ -1,14 +1,14 @@
-import { Text } from "react-native";
+import { Text as RNText } from "react-native";
 
 export function ColoredText(props: { text?: string | number; bgColor: string }) {
   const { text, bgColor } = props;
   return (
-    <Text
+    <RNText
       style={{
         color: bgColor === `#000000` ? `#ffffff` : `#000000`,
       }}
     >
       {text}
-    </Text>
+    </RNText>
   );
 }
