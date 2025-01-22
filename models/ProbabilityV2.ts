24bf5c2720b47b5d40d0c93f177c0283db85cf08
FlixonStudios <chiakaiming@gmail.com>
2025-01-22 21:36:59 +0800

    add prob v2 helpers

--- models/ProbabilityV2.ts
+++ models/ProbabilityV2.ts
@@ -1,8 +1,38 @@
 import { Permutation } from "js-combinatorics";
 
+interface ReducerResults {
+  value: number;
+  success?
+  : boolean;
+}
+
 export class ProbabilityV2 {
   public getAllPermutations(inputArr: string[]) {
     const permutations = new Permutation(inputArr);
     return [...permutations];
+  }
+  
+  public getAverageValue(
+    permutations: string[][],
+    reducer: (set: string[]) => ReducerResults
+  ) {
+    const value = permutations.reduce(
+      (total, set) => (total += reducer(set).value),
+      0
+    );
+
+    return (value / permutations.length).toPrecision(4);
+  }
+
+  public getAverageOptimisedValue(
+    permutations: string[],
+    reducer: (set: string) => ReducerResults
+  ) {
+    const value = permutations.reduce(
+      (total, set) => (total += reducer(set).value),
+      0
+    );
+
+    return (value / permutations.length).toPrecision(4);
   }
 }
--- models/__tests__/ProbabilityV2.spec.ts
+++ models/__tests__/ProbabilityV2.spec.ts
@@ -1,20 +1,22 @@
 import { ProbabilityV2 } from "../ProbabilityV2";
 
 describe("ProbabilityV2", () => {
-  it("should return all possible combinations given the cards", () => {
-    const mockCards = ["0", "1", "2"];
+  describe("getAllPermutations", () => {
+    it("should return all possible combinations given the cards", () => {
+      const mockCards = ["0", "1", "2"];
 
-    const p = new ProbabilityV2();
-    const result = p.getAllPermutations(mockCards);
-    const expected = [
-      ["0", "1", "2"],
-      ["0", "2", "1"],
-      ["1", "0", "2"],
-      ["1", "2", "0"],
-      ["2", "0", "1"],
-      ["2", "1", "0"],
-    ];
+      const p = new ProbabilityV2();
+      const result = p.getAllPermutations(mockCards);
+      const expected = [
+        ["0", "1", "2"],
+        ["0", "2", "1"],
+        ["1", "0", "2"],
+        ["1", "2", "0"],
+        ["2", "0", "1"],
+        ["2", "1", "0"],
+      ];
 
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
   });
 });
